b828074162d1dd8cf86c9ed380f5b1e2b2da766e
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2017-10-23 09:49:24 -0400

    Fix dependencies.
    Trailing .0 is not allowed by the version spec.

--- info.rkt
+++ info.rkt
@@ -2,5 +2,5 @@
 (define version 0.2)
 (define collection "ramunk")
 (define categories '(game physics-engine ffi))
-(define deps '(("autoffi" "0.2.0")))
+(define deps '(("autoffi" "0.2")))
 (define compile-omit-paths '("examples" "bin" "chipmunk2d" "chipmunk-release"))
